b1f8c8f6beab30543e2536377aab30f2301ac9ab
AdrianRome <adrian.romero@the-cocktail.com>
2019-12-15 20:28:02 +0100

    adri

--- AdriR.R
+++ AdriR.R
@@ -8,8 +8,7 @@ library()
 
 
 
-weatherAUS <- read_csv("weatherAUS.csv")
-weatherAUS <- filter(weatherAUS, Location == "Sydney")
+weatherAUS <- read_csv("weatherAUS_2.csv")
 weatherAUS_WindDir9am <- select(weatherAUS,WindDir9am)
 weatherAUS_WindDir9am
 
@@ -90,3 +89,109 @@ ggplot(weatherAUS, aes(x = WindSpeed9am)) + geom_density() + ggtitle('Función d
 ggplot(weatherAUS, aes(y=WindSpeed9am)) +  geom_boxplot() + ggtitle('Boxplot de WindSpeed9am')
 # Basic scatter plot
 ggplot(weatherAUS, aes(x=WindSpeed9am, y=RainTomorrow)) + geom_point() 
+
+## WindSpeed3pm
+
+var_WindSpeed3pm = weatherAUS$WindSpeed3pm
+#Medidas de centralidad
+summary(var_WindSpeed3pm)
+#medidas de dispersión
+medidas_dispersion<-function(x) {
+  return(list('rango'= range(x), 'Varianza'= var(x), 'Desciación_tipica'= sd(x)))}
+#Devuelve NAs porque hay NAs en la columna.
+medidas_dispersion(var_WindSpeed3pm)
+
+length(var_WindSpeed3pm)
+# Basic histogram
+ggplot(weatherAUS, aes(x=WindSpeed3pm)) + geom_histogram() + ggtitle('Histograma de WindSpeed3pm')
+# Basic Density plot
+ggplot(weatherAUS, aes(x = WindSpeed3pm)) + geom_density() + ggtitle('Función de densidad de WindSpeed3pm')
+# Basic box plot
+ggplot(weatherAUS, aes(y=WindSpeed3pm)) +  geom_boxplot() + ggtitle('Boxplot de WindSpeed3pm')
+# Basic scatter plot
+ggplot(weatherAUS, aes(x=WindSpeed3pm, y=RainTomorrow)) + geom_point() 
+
+## Humidity9am
+
+var_Humidity9am = weatherAUS$Humidity9am
+#Medidas de centralidad
+summary(var_Humidity9am)
+#medidas de dispersión
+medidas_dispersion<-function(x) {
+  return(list('rango'= range(x), 'Varianza'= var(x), 'Desciación_tipica'= sd(x)))}
+#Devuelve NAs porque hay NAs en la columna.
+medidas_dispersion(var_Humidity9am)
+
+length(var_Humidity9am)
+# Basic histogram
+ggplot(weatherAUS, aes(x=Humidity9am)) + geom_histogram() + ggtitle('Histograma de Humidity9am')
+# Basic Density plot
+ggplot(weatherAUS, aes(x = Humidity9am)) + geom_density() + ggtitle('Función de densidad de Humidity9am')
+# Basic box plot
+ggplot(weatherAUS, aes(y=Humidity9am)) +  geom_boxplot() + ggtitle('Boxplot de Humidity9am')
+# Basic scatter plot
+ggplot(weatherAUS, aes(x=Humidity9am, y=RainTomorrow)) + geom_point() 
+
+## Humidity3pm
+
+var_Humidity3pm = weatherAUS$Humidity3pm
+#Medidas de centralidad
+summary(var_Humidity3pm)
+#medidas de dispersión
+medidas_dispersion<-function(x) {
+  return(list('rango'= range(x), 'Varianza'= var(x), 'Desciación_tipica'= sd(x)))}
+#Devuelve NAs porque hay NAs en la columna.
+medidas_dispersion(var_Humidity3pm)
+
+length(var_Humidity3pm)
+# Basic histogram
+ggplot(weatherAUS, aes(x=Humidity3pm)) + geom_histogram() + ggtitle('Histograma de Humidity3pm')
+# Basic Density plot
+ggplot(weatherAUS, aes(x = Humidity3pm)) + geom_density() + ggtitle('Función de densidad de Humidity3pm')
+# Basic box plot
+ggplot(weatherAUS, aes(y=Humidity3pm)) +  geom_boxplot() + ggtitle('Boxplot de Humidity3pm')
+# Basic scatter plot
+ggplot(weatherAUS, aes(x=Humidity3pm, y=RainTomorrow)) + geom_point() 
+
+## Pressure9am
+
+var_Pressure9am = weatherAUS$Pressure9am
+#Medidas de centralidad
+summary(var_Pressure9am)
+#medidas de dispersión
+medidas_dispersion<-function(x) {
+  return(list('rango'= range(x), 'Varianza'= var(x), 'Desciación_tipica'= sd(x)))}
+#Devuelve NAs porque hay NAs en la columna.
+medidas_dispersion(var_Pressure9am)
+
+length(var_Pressure9am)
+# Basic histogram
+ggplot(weatherAUS, aes(x=Pressure9am)) + geom_histogram() + ggtitle('Histograma de Pressure9am')
+# Basic Density plot
+ggplot(weatherAUS, aes(x = Pressure9am)) + geom_density() + ggtitle('Función de densidad de Pressure9am')
+# Basic box plot
+ggplot(weatherAUS, aes(y=Pressure9am)) +  geom_boxplot() + ggtitle('Boxplot de Pressure9am')
+# Basic scatter plot
+ggplot(weatherAUS, aes(x=Pressure9am, y=RainTomorrow)) + geom_point() 
+
+## Pressure3pm
+
+var_Pressure3pm = weatherAUS$Pressure3pm
+#Medidas de centralidad
+summary(var_Pressure3pm)
+#medidas de dispersión
+medidas_dispersion<-function(x) {
+  return(list('rango'= range(x), 'Varianza'= var(x), 'Desciación_tipica'= sd(x)))}
+#Devuelve NAs porque hay NAs en la columna.
+medidas_dispersion(var_Pressure3pm)
+
+length(var_Pressure3pm)
+# Basic histogram
+ggplot(weatherAUS, aes(x=Pressure3pm)) + geom_histogram() + ggtitle('Histograma de Pressure3pm')
+# Basic Density plot
+ggplot(weatherAUS, aes(x = Pressure3pm)) + geom_density() + ggtitle('Función de densidad de Pressure3pm')
+# Basic box plot
+ggplot(weatherAUS, aes(y=Pressure3pm)) +  geom_boxplot() + ggtitle('Boxplot de Pressure3pm')
+# Basic scatter plot
+ggplot(weatherAUS, aes(x=Pressure3pm, y=RainTomorrow)) + geom_point() 
+
